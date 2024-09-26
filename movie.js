d25956468c5cb48fb59250f493a13ddfe05a3c1d
unknown <wizzylive16@gmail.com>
2024-09-26 11:57:21 +0100

    first commit

--- movie.js
+++ movie.js
@@ -23,19 +23,58 @@ async function movieDisplay(){
         document.querySelector('.movie-grid').appendChild(myDiv)
     })
 }
+async function tvDisplay(){
+    const {results} =await fetchMovie('tv/popular');
+    results.forEach(tv_Show =>{
+        const myDiv = document.createElement('div');
+        myDiv.classList.add('tiles');
+        myDiv.innerHTML =  `
+              <a href="movie-details.html?id=${tv_Show.id}">
+                ${
+                    tv_Show.poster_path ?
+                    `<img src="https://image.tmdb.org/t/p/w300${tv_Show.poster_path}" alt=${tv_Show.name}>`
+                    :`<img src="./images/no-image.jpg" alt="${tv_Show.name}">`
+                }
+                <div class="content">
+                    <h3>${tv_Show.name}</h3>
+                    <p><strong>Release</strong>: ${tv_Show.first_air_date}</p>
+                </div>
+            </a>
+        `
+        document.querySelector('.movie-grid').appendChild(myDiv)
+    })
+}
+
+
 
 const movie_Details = async ()=>{
-    const movieDetail = await fetchMovie('movie/{movie_id}');
-    console.log(movieDetail);
+    const getId = window.location.search.split('=')[1]
+    console.log(getId);
+}
+
+
+function showSpinner(){
+    document.querySelector('.spinner').classList.remove('show');
+    document.querySelector('.pagination').style.display = 'none';
+}
+function hideSpinner(){
+    document.querySelector('.spinner').classList.add('show');
+    const pagination = document.querySelector('.pagination');
+    pagination.style.display = 'flex';
+    pagination.style.justifyContent = 'center';
+    pagination.style.flexDirection = 'column';
+    pagination.style.alignItems = 'center'
+
 }
 
 async function fetchMovie(movies_endpoint) {
     const API_Key = '3c8685db24642e4c2b971877340f4ae9';
     const API_URL = 'https://api.themoviedb.org/3/';
-
+    showSpinner()
     const res = await fetch(`${API_URL}${movies_endpoint}?api_key=${API_Key}&language=en-US`);
 
     const data = await res.json();
+    hideSpinner()
     return data;
 }
 
@@ -52,8 +91,6 @@ function activeLink(){
 
 
 function init(){
-    const spinner = document.querySelector('.spinner');
-    spinner.style.display = 'none';
     switch (global.currentPage){
         case '/home.html':
         case '/':
@@ -63,7 +100,7 @@ function init(){
             console.log('tv-detail');
         break;
         case '/tv-shows.html':
-            console.log('tv-show');
+            tvDisplay();
         break;
         case '/movie-details.html':
             movie_Details();
